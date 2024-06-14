a0ab015eb581b915c2465eead14315fefc03be51
riporippo <22fi124@ms.dendai.ac.jp>
2024-06-14 10:19:27 +0900

    追加

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from typing import Optional
 
 from fastapi import FastAPI
+from random import Random
+from pydantic import BaseModel
+
+class AreaOfCircle(BaseModel):
+    r: float
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
@@ -12,3 +16,22 @@ async def root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/omikuji")
+def omikuji():
+    omikuji_list = [
+        "大吉",
+        "中吉",
+        "小吉",
+        "吉",
+        "末吉",
+        "凶",
+        "小凶",
+        "大凶",
+    ]
+    return omikuji_list[Random.randrange(10)]
+
+@app.post("/areaOfCircle/")
+async def calcAreaOfCircle(circle: AreaOfCircle):
+    area = circle.r ** 2 * 3.1415
+    return {"radius": circle.r, "area": area}
