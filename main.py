0bbadbd00119be2bfccda491dfc0f2255d219afd
riporippo <22fi124@ms.dendai.ac.jp>
2024-06-14 12:02:35 +0900

    indexの中身変更

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from typing import Optional
 from fastapi import FastAPI
 from random import Random
 from pydantic import BaseModel
+from fastapi import HTMLResponse
 
 class AreaOfCircle(BaseModel):
     r: float
@@ -12,6 +13,32 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/index")
+def index():
+    html_content = """
+    <DOCTYPE! html>
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <title>こんにちは！</title>
+            p#ehehe{
+                background-color: #ffffff;
+                color: #000000;
+                font-size: 1.5rem;
+            }
+        </head>
+        <body>
+        <p>
+            <h1>うおおおお！WEBAPI!WEBAPI!</h1>
+        </p>
+        <p id="ehehe">
+            みってるぅ～？（笑）
+        </p>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
@@ -30,7 +57,7 @@ def omikuji():
         "小凶",
         "大凶",
     ]
-    return omikron_list[random_number.randint(0,7)]
+    return {"result":omikron_list[random_number.randint(0,7)]}
 
 @app.post("/areaOfCircle/")
 async def calcAreaOfCircle(circle: AreaOfCircle):
